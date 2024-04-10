f512d584924ee993fac786a3325c1aea8ad62f34
MusiliC <musilibrian07@gmail.com>
2024-04-10 22:06:43 +0300

    Spring security setup using custom tables and custom names

--- src/main/java/com/ceetech/mycoolapp/security/DemoSecurityConfig.java
+++ src/main/java/com/ceetech/mycoolapp/security/DemoSecurityConfig.java
@@ -1,60 +1,79 @@
 package com.ceetech.mycoolapp.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+        // add support for jdbc
 
-        UserDetails john = User.builder()
-                .username("John")
-                .password("{noop}1234")
-                .roles("EMPLOYEE")
-                .build();
+        @Bean
+        public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}1234")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+                JdbcUserDetailsManager jUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}1234")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+                // define query to retrieve user by username
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
+                jUserDetailsManager
+                                .setUsersByUsernameQuery("select user_id, pw, active from members where user_id = ?");
 
-    }
+                // define query to retrieve roles by username
+                jUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id = ?");
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                return jUserDetailsManager;
+        }
 
-        http.authorizeHttpRequests(config -> config
-                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        // http Basic Authentication
-        http.httpBasic(Customizer.withDefaults());
+                http.authorizeHttpRequests(config -> config
+                                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
 
-        // disable CSRF
-        http.csrf(csrf -> csrf.disable());
+                // http Basic Authentication
+                http.httpBasic(Customizer.withDefaults());
 
-        return http.build();
-    }
+                // disable CSRF
+                http.csrf(csrf -> csrf.disable());
+
+                return http.build();
+        }
 
 }
+
+// @Bean
+// public InMemoryUserDetailsManager userDetailsManager() {
+
+// // UserDetails john = User.builder()
+// // .username("John")
+// // .password("{noop}1234")
+// // .roles("EMPLOYEE")
+// // .build();
+
+// // UserDetails mary = User.builder()
+// // .username("mary")
+// // .password("{noop}1234")
+// // .roles("EMPLOYEE", "MANAGER")
+// // .build();
+
+// // UserDetails susan = User.builder()
+// // .username("susan")
+// // .password("{noop}1234")
+// // .roles("EMPLOYEE", "MANAGER", "ADMIN")
+// // .build();
+
+// return new InMemoryUserDetailsManager(john, mary, susan);
+
+// }
